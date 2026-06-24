281130c8eac751a1ac99af2e41c60464c637c03e
OkCom <90555600+1156666@users.noreply.github.com>
2026-06-24 16:35:58 +0800

    fuck you I forgot commiting

--- src/mem/collection.rs
+++ src/mem/collection.rs
@@ -1,48 +1,54 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::str::from_utf8;
 use crate::data_source::data::{DataSource, PADDING_SIZE};
 
-const TOTAL_MEMORY_INFO:usize = 0;
-const AVAILABLE_MEMORY_INFO:usize = 2;
+const THIS_IS_TOTAL_MEMORY_INFO:usize = 0;
+const THIS_IS_AVAILABLE_MEMORY_INFO:usize = 2;
+const MEMORY_TOTAL_START:usize = 0;
+const MEMORY_TOTAL_END:usize = MEMORY_TOTAL_START + PADDING_SIZE;
+const MEMORY_AVAIL_START:usize = PADDING_SIZE;
+const MEMORY_AVAIL_END:usize = MEMORY_AVAIL_START + PADDING_SIZE;
 pub fn read_mem_info(source:&mut DataSource) {
     let data_source = &mut source.public_array;
     
     if let Ok(file) = File::open("/proc/meminfo") {
         let reader = BufReader::new(file);
-        for (number, info) in reader.lines().take(3).enumerate() {
+        for (THIS_INFO, info) in reader.lines().take(3).enumerate() {
             if let Ok(info) = info {
-                let actual_length = info.len();
-                let byte_char = info.as_bytes();
-                let mut buffer_padding = [0u8; PADDING_SIZE];
-                buffer_padding[0..actual_length].copy_from_slice(&byte_char[0..actual_length]);
-                match number {
-                    TOTAL_MEMORY_INFO
-                    => data_source[0..PADDING_SIZE].copy_from_slice(&buffer_padding),
+                let mut data = info.split_whitespace();
+                data.next();
+                let mut number:u64 = 0;
 
-                    AVAILABLE_MEMORY_INFO
-                    => data_source[PADDING_SIZE..2 * PADDING_SIZE].copy_from_slice(&buffer_padding),
+                for(index, slice) in data.enumerate(){
+                    if let Ok(val) = slice.parse::<u64>(){
+                        if index == 0{ number = val }
+                    }
+                }
+                let byte_char = number.to_be_bytes();
+                let length = byte_char.len();
+                match THIS_INFO {
+                    THIS_IS_TOTAL_MEMORY_INFO
+                    => {
+                        data_source[MEMORY_TOTAL_START..MEMORY_TOTAL_END].fill(0);
+                        data_source[MEMORY_TOTAL_START..MEMORY_TOTAL_START + length].copy_from_slice(&byte_char)
+                    },
+
+                    THIS_IS_AVAILABLE_MEMORY_INFO
+                    => {
+                        data_source[MEMORY_AVAIL_START..MEMORY_AVAIL_END].fill(0);
+                        data_source[MEMORY_AVAIL_START..MEMORY_AVAIL_START + length].copy_from_slice(&byte_char)
+                    },
 
                     _
                     => {}
                 }
 
-                match number {
-                    TOTAL_MEMORY_INFO
-                    => {
-                        println!("-----------");
-                        println!("{}", from_utf8(&data_source[0..PADDING_SIZE]).unwrap());
-                        println!("-----------");
-                    },
-                    AVAILABLE_MEMORY_INFO
-                    => {
-                        println!("-----------");
-                        println!("{}", from_utf8(&data_source[PADDING_SIZE..2 * PADDING_SIZE]).unwrap());
-                        println!("-----------");
-                    },
-                    _ => {}
-                }
+
             }
         }
     }
+    let check_total_memory = u64::from_be_bytes(data_source[MEMORY_TOTAL_START..MEMORY_TOTAL_START + 8].try_into().unwrap());
+    let check_avail_memory = u64::from_be_bytes(data_source[MEMORY_AVAIL_START..MEMORY_AVAIL_START + 8].try_into().unwrap());
+    println!("MEMORY TOTAL : {}, MEMORY AVAIL: {}", check_total_memory, &check_avail_memory)
+
 }
